771065aef81e6bd9625bb5a6babf153fe0faa167
Diego Vicente <diegovicente@protonmail.com>
2017-12-01 16:02:23 +0100

    Day 1 first problem solved

---
+++ src/puzzle-1.hs
@@ -0,0 +1,10 @@
+import Data.Char (digitToInt)
+import Data.List (splitAt)
+
+-- Solve the problem 1.1
+captcha :: String -> Int
+captcha str = solve $ (map digitToInt) (str ++ [str !! 0])
+  where solve [] = 0
+        solve (_:[]) = 0
+        solve (y:x:xs) = if x == y then x + solve (x:xs)
+                         else solve (x:xs)
