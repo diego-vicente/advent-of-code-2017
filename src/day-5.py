2a5213a5fa50e3a7614ab78ac82e8f470de03169
Diego Vicente <diegovicente@protonmail.com>
2017-12-05 15:31:46 +0100

    Problem 2 solved

--- src/day-5.py
+++ src/day-5.py
@@ -1,8 +1,11 @@
+from copy import copy
+
 def main():
     with open('src/maze.txt', 'r') as f:
-        maze = map(int, f.readlines())
+        maze = list(map(int, f.readlines()))
 
-    print('Solution to problem 1 is', steps_1(list(maze)))
+    print('Solution to problem 1 is', steps_1(copy(maze)))
+    print('Solution to problem 2 is', steps_2(maze))
 
 def steps_1(maze):
     """Steps taken to solve the maze using rules in problem 1."""
@@ -12,6 +15,23 @@ def steps_1(maze):
     while current < len(maze):
         offset = maze[current]
         maze[current] += 1
+        current += offset
+        steps += 1
+
+    return steps
+
+def steps_2(maze):
+    """Steps taken to solve the maze using rules in problem 2."""
+    current = 0
+    steps = 0
+    offset = 0
+
+    while current < len(maze):
+        offset = maze[current]
+        if maze[current] >= 3:
+            maze[current] -= 1
+        else:
+            maze[current] += 1
         current += offset
         steps += 1
 
