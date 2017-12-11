105f5c0b867962068e8f96b9ef9ccd15192b0b22
Diego Vicente <diegovicente@protonmail.com>
2017-12-11 20:24:01 +0100

    Day 7 problem 2 solved

--- src/day-7.py
+++ src/day-7.py
@@ -1,45 +1,90 @@
 import re
 
+
+class Node:
+    def __init__(self, name, weight, children):
+        self.name = name
+        self.weight = weight
+        self.children = children
+
 def main():
-    print('Solution to problem 1 is', locate_root('src/day-7.txt'))
+    root = build_tree('src/day-7.txt')
+    print('Solution to problem 1 is', root.name)
+    balance_weigths(root)
 
-def parse_line(line):
-    """Process a line into tuples."""
-    leaf = re.match(r"(\w+) \((\d+)\) -> (.+)", line)
-    root = re.match(r"(\w+) \((\d+)\)", line)
 
-    if leaf:
-        return leaf.group(1), leaf.group(2), leaf.group(3).split(', ')
-    elif root:
-        return root.group(1), root.group(2)
-
-def locate_root(filename):
+def build_tree(filename):
+    """Parse a text file and build the associated tree."""
     root_candidates = []
     total_leaves = []
+    node_dict = {}
 
+    # Read file
     with open(filename, 'r') as f:
-        nodes = f.readlines()
+        raw_nodes = f.readlines()
 
-    for node in nodes:
-        elements = parse_line(node)
+    # Parse lines and locate root
+    for raw_node in raw_nodes:
+        elements = parse_line(raw_node)
         if len(elements) == 2:
-            root, _ = elements
+            name, weight = elements
             leaves = None
         elif len(elements) == 3:
-            root, _, leaves = elements
+            name, weight, leaves = elements
             for leaf in leaves:
                 if leaf in root_candidates:
                     root_candidates.remove(leaf)
             total_leaves += leaves
 
+        if name not in total_leaves:
+            root_candidates.append(name)
 
-        if root not in total_leaves:
-            root_candidates.append(root)
+        node_dict[name] = Node(name, int(weight), leaves)
 
-    if len(root_candidates) == 1:
-        return root_candidates[0]
-    else:
-        return None
+    # Build tree recursively from root
+    root = node_dict[root_candidates[0]]
+    connect_children(root, node_dict)
+
+    return root
+
+
+def connect_children(father, node_dict):
+    """Recursively build a tree from parsed lines."""
+    if father.children:
+        father.children = list(
+            map(lambda x: node_dict[x], father.children)
+        )
+
+        for child in father.children:
+            connect_children(child, node_dict)
+
+
+def parse_line(line):
+    """Process a line into tuples."""
+    leaf = re.match(r"(\w+) \((\d+)\) -> (.+)", line)
+    name = re.match(r"(\w+) \((\d+)\)", line)
+
+    if leaf:
+        return leaf.group(1), leaf.group(2), leaf.group(3).split(', ')
+    elif name:
+        return name.group(1), name.group(2)
+
+
+def balance_weigths(node):
+    """Locate the the problematic node and fix the weights recursively."""
+    weights = []
+    if not node.children:
+        return node.weight
+
+    for child in node.children:
+        weights.append(balance_weigths(child))
+
+    if not len(set(weights)) == 1:
+        print('Unbalanced node: ', weights)
+        print(list(map(lambda x: x.weight, node.children)))
+        return node.weight + sorted(weights)[0] * len(weights)
+
+    return node.weight + sum(weights)
 
 if __name__ == '__main__':
     main()
