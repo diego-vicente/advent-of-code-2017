7c7ccadb90af0756efaa06bceb472758819c018d
Diego Vicente <diegovicente@protonmail.com>
2017-12-12 19:17:07 +0100

    Solution to day 10 - 2

--- src/day-10.py
+++ src/day-10.py
@@ -2,18 +2,21 @@ import numpy as np
 
 
 def main():
-    problem_A = knot_hash_round(
-        [34, 88, 2, 222, 254, 93, 150, 0, 199, 255, 39, 32, 137, 136, 1, 167]
+    problem_A, _, _ = knot_hash_round(
+        np.arange(256),
+        [34, 88, 2, 222, 254, 93, 150, 0, 199, 255, 39, 32, 137, 136, 1, 167],
+        0, 0
     )
+
     print('First solution is', problem_A[0] * problem_A[1])
 
 
-def knot_hash_round(lengths):
-    """Perform a single round of the knot hash."""
-    circular_list = np.arange(256)
+    problem_B = knot_hash('34,88,2,222,254,93,150,0,199,255,39,32,137,136,1,167')
+    print('Second solution is', problem_B)
 
-    skip_size = 0
-    pos = 0
+
+def knot_hash_round(circular_list, lengths, skip_size, pos):
+    """Perform a single round of the knot hash."""
 
     for length in lengths:
         indices = np.arange(pos, pos + length) % len(circular_list)
@@ -21,7 +24,31 @@ def knot_hash_round(lengths):
         pos = (pos + length + skip_size) % len(circular_list)
         skip_size += 1
 
-    return circular_list
+    return circular_list, pos, skip_size
+
+def knot_hash(string):
+    """Obtain the knot hash of a certain string"""
+
+    # Obtain the lengths from the ASCII codes
+    lengths = list(map(ord, string)) + [17, 31, 73, 47, 23]
+
+    # Initial values of the hash
+    c_list = np.arange(256)
+    skip_size = 0
+    pos = 0
+
+    # Run 64 rounds on the list
+    for _ in range(64):
+        c_list, pos, skip_size = knot_hash_round(c_list, lengths, skip_size, pos)
+
+    # Compute 16 bins of reduced XOR
+    indices = np.arange(0, 255, 16)
+    bins = np.bitwise_xor.reduceat(c_list, indices)
+
+    # Return the hexadecimal string representation of the dense hash
+    return ''.join('{:02x}'.format(x) for x in bins)
+
+
 
 
 if __name__ == '__main__':
