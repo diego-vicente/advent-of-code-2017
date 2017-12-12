b92b320be83edf4d391f41bc1e10e57bc806b74c
Diego Vicente <diegovicente@protonmail.com>
2017-12-12 20:23:43 +0100

    Solution to day 11 - 2

--- src/day-11.py
+++ src/day-11.py
@@ -4,7 +4,9 @@ def main():
     with open('src/day-11.txt', 'r') as f:
         steps = f.read().replace('\n', '').split(',')
 
-    print(hex_path(steps))
+    a, b = hex_path(steps)
+    print('Solution to problem 1 is', a)
+    print('Solution to problem 2 is', b)
 
 def hex_path(steps):
     """Walk a hex-grid path and return the final distance from the origin."""
@@ -19,10 +21,13 @@ def hex_path(steps):
         'sw' : np.array([-1, 0, 1]),
     }
 
+    distances = []
+
     for step in steps:
         current += directions[step]
+        distances.append(np.abs(current).sum() / 2)
 
-    return np.abs(current).sum() / 2
+    return distances[-1], max(distances)
 
 if __name__ == '__main__':
    main()
