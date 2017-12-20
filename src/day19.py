cc99b360d8a96f25f7cbfe65a4b227f0799d9e06
Diego Vicente <diegovicente@protonmail.com>
2017-12-20 20:13:51 +0100

    Solution to 19 - 1

---
+++ src/day19.py
@@ -0,0 +1,54 @@
+# Set of directions needed to move the vector.
+Directions = {
+    'Left': (-1, 0),
+    'Right': (1, 0),
+    'Up': (0, -1),
+    'Down': (0, 1),
+}
+
+def main():
+    with open('src/day-19.txt', 'r') as f:
+        diagram = f.read().splitlines()
+
+    print(traverse_diagram(diagram))
+
+def traverse_diagram(diagram):
+    """Return the nodes by traversing the diagram."""
+    direction = 'Down'
+    dead_end = False
+    path = []
+
+    # Find the starting point
+    x, y = (diagram[0].index('|'), 0)
+
+    while not dead_end:
+        move = Directions[direction]
+        x, y = x + move[0], y + move[1]
+        next_step = diagram[y][x]
+
+        if next_step == '|' or next_step == '-':
+            continue
+        elif next_step == '+':
+            if direction == 'Left' or direction == 'Right':
+                try:
+                    direction = 'Down' if diagram[y + 1][x] == '|' else 'Up'
+                except IndexError:
+                    direction = 'Up'
+            elif direction == 'Down' or direction == 'Up':
+                try:
+                    direction = 'Right' if diagram[y][x + 1] == '-' else 'Left'
+                except IndexError:
+                    direction = 'Left'
+        elif next_step.isalpha():
+            path.append(next_step)
+        else:
+            dead_end = True
+
+    return str(path)
+
+
+
+
+
+if __name__ == '__main__':
+    main()
