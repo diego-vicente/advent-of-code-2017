eb950406ade4e9ef2687b784ea0bd29c579a4461
Diego Vicente <diegovicente@protonmail.com>
2017-12-23 22:08:04 +0100

    Had a bad time but found solution 23 - 2

--- src/day23.py
+++ src/day23.py
@@ -66,6 +66,55 @@ def main():
         program.run_instruction()
 
     print('Solution to problem 1 is', program.mul_count)
+    print('Solution to problem 2 is', program_optimized())
+
+def program_translated():
+    """Python equivalent of the Duet code.
+
+    Just never run this code. It is not going to stop. However, it is enough to
+    understand that its purpose is to count the number of composite numbers in
+    [b, c, 17].
+
+    """
+    b = 100 * 67 + 100000
+    c = b + 17000
+    h = 0
+
+    while True:
+        d = 2
+        while d != b:
+            e = 2
+            while e != b:
+                if d * e == b:
+                    h += 1
+                    break
+                e += 1
+            d += 1
+        if b == c:
+            return h
+        b += 17
+
+def program_optimized():
+    """The real Pythonic way to compute the problem 2.
+
+    Compute efficiently the primes up to c, then substract those to get the
+    number of composite numbers.
+
+    """
+    b = 100 * 67 + 100000
+    c = b + 17000
+
+    h = 0
+    for n in range(b, c+1, 17):
+        for k in range(2 , ceil(sqrt(n))):
+            if n % k == 0:
+                h += 1
+                break
+
+    return h
+
+
+
 
 if __name__ == '__main__':
     main()
