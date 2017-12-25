ed35d6bb6cfd321c4cc63f435a125d9e26bd50eb
Diego Vicente <diegovicente@protonmail.com>
2017-12-25 12:50:24 +0100

    Solution to day 1 - 2

--- src/day1.py
+++ src/day1.py
@@ -6,19 +6,29 @@ def pairwise(iterable):
     next(b, None)
     return zip(a, b)
 
+def antipode(lst):
+    """Generator that returns the opposing elements in a circular list."""
+    half = len(lst) // 2
+    for i, elem in enumerate(lst):
+        yield elem, lst[(i + half) % len(lst)]
+
 
 def main():
     with open('data/day-1.txt') as f:
         captcha = list(f.read().splitlines()[0])
 
-    # Add the first element to the end of the list
-    captcha.append(captcha[0])
-
     print('Solution to problem 1 is', sum_captcha(captcha))
+    print('Solution to problem 2 is', circular_captcha(captcha))
+
 
 def sum_captcha(captcha):
     """Sum the numbers that are equal to the next one in the captcha."""
-    return sum(int(a) for a, b in pairwise(captcha) if a == b)
+    return sum(int(a) for a, b in pairwise(captcha + [captcha[0]]) if a == b)
+
+def circular_captcha(captcha):
+    """Sum the numbers that are equal to the next one in the captcha."""
+    return sum(int(a) for a, b in antipode(captcha) if a == b)
+
 
 if __name__ == '__main__':
     main()
